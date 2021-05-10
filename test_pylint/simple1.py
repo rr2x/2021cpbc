8067a5e5a804e53db44c2d0c470bb02c2e2ddf0c
rr2x <79620740+rr2x@users.noreply.github.com>
2021-05-10 17:41:20 +0800

    pylint and unittest

---
+++ test_pylint/simple1.py
@@ -0,0 +1,21 @@
+# a = 1
+# b = 2
+# print(a)
+# print(B)
+
+'''
+a very simple script
+'''
+
+
+def myfunc():
+    '''
+    a simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+myfunc()
---
+++ test_unittest/cap.py
@@ -0,0 +1,4 @@
+def cap_text(text):
+    # .capitalize() only capitalizes the 1st letter
+    # .title() will capitalize 1st letter of every word
+    return text.title()
