598f3e4e8072d362bd772c3f075fcc1c16b31385
rr2x <79620740+rr2x@users.noreply.github.com>
2021-05-11 09:07:47 +0800

    milestone project 2 (war card game), card/deck/player class

--- creating-modules/myprogram.py
+++ creating-modules/myprogram.py
@@ -1,9 +1,9 @@
-# <file> ...<function>
+# from <file> import <function>
 # from mymodule import my_func
 # my_func()
 
-# <folder> ...<file>
 # important to have an empty __init__.py per folder
+# from <folder>.<subfolder> import <file>
 from MyMainPackage import some_main_script
 from MyMainPackage.SubPackage import mysubscript
 
--- test_unittest/cap.py
+++ test_unittest/cap.py
@@ -1,4 +1,5 @@
-def cap_text(text):
+def cap_text(text: str):
     # .capitalize() only capitalizes the 1st letter
     # .title() will capitalize 1st letter of every word
+
     return text.title()
